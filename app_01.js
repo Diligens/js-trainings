d193a0993ee40642c699057b7af7de9be74482d1
Diligens <artemrandom@yandex.ru>
2019-04-18 19:32:03 +0300

    IIFE & Closures, Call & Apply, Bind

---
+++ app_01.js
@@ -0,0 +1,21 @@
+let app = (function(){
+  let name = "IIFE and Closures";
+  let status = "In progress";
+
+  let getName = function() {
+    return name;
+  };
+  let getStatus = function() {
+    return status;
+  };
+
+  return {
+    getName: getName,
+    getStatus: getStatus
+  };
+})();
+
+console.log("============ APP 01 LOGS START ============");
+
+console.log(app.getName());
+console.log(app.getStatus());
---
+++ app_02.js
@@ -0,0 +1,20 @@
+let o = {
+  carId: 123,
+  getId: function(arg) {
+    return this.carId + " " + arg;
+  }
+};
+
+let newCar = { 
+  carId: 456 
+}; 
+
+let numbers = [1, 2, 419, 234, 21, 0, -3, 9];
+
+let newFunc = o.getId.bind(newCar)
+
+console.log("============ APP 02 LOGS START ============");
+
+console.log(o.getId.call(newCar, "of newCar")); // Call: create new context for function getId
+console.log(Math.max.apply(null, numbers)); // Apply: get max number. At the same context, but arguments in array
+console.log(newFunc()); // Copy method/function from object o to newFunc

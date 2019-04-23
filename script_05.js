63c449ad80a10dd45bfce61608b9725142b4436d
Diligens <artemrandom@yandex.ru>
2019-04-23 18:13:29 +0300

    JSON, stringify, parse

--- script_05.js
+++ script_05.js
@@ -1,2 +1,17 @@
+let jsonIn = `
+  [
+    {"userId" : 1},
+    {"userId" : 2},
+    {"userId" : 3}
+  ]
+`
+
+let userIds = JSON.parse(jsonIn);
+
 console.log("============ SCRIPT 05 LOGS START ============");
 
+console.log('after JSON.parse: ');
+console.log(userIds);
+userIds = JSON.stringify(userIds);
+console.log('after JSON.stringify: ');
+console.log(userIds);

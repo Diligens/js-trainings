97321807e0b69cca88837166240f2ac6cdab798e
Diligens <artemrandom@yandex.ru>
2019-04-25 11:10:22 +0300

    window Object

---
+++ scripts/script_10.js
@@ -0,0 +1,8 @@
+import { Car } from './models/car';
+
+console.log("============ SCRIPT 10 LOGS START ============");
+
+window.SUPERSOMETHING = true;
+// SOMETHINGNOTGOOD = true;
+
+console.log(window);

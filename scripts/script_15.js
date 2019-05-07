876fcc8555063a8d4e21a30e562c0c76d761cf4f
Diligens <artemrandom@yandex.ru>
2019-05-07 21:17:49 +0300

    Forms. Preventing Form Submission

---
+++ scripts/script_15.js
@@ -0,0 +1,9 @@
+
+console.log("============ SCRIPT 15 LOGS START ============");
+
+let form = document.getElementById('user-form');
+
+form.addEventListener('submit', event => {
+    // prevent the browser from submitting the form
+    event.preventDefault();
+});

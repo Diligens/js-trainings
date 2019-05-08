7b10ac45aa7b86d181bfd51139e771f986aab87e
Diligens <artemrandom@yandex.ru>
2019-05-08 18:04:14 +0300

    Showing Validation Errors

--- scripts/script_15.js
+++ scripts/script_15.js
@@ -6,8 +6,17 @@ let form = document.getElementById('user-form');
 form.addEventListener('submit', event => {
     // prevent the browser from submitting the form
     let user = form.elements['user'];
+    let userError = document.getElementById('user-error');
+    
     let avatarFile = form.elements['avatar-file'];
 
-    console.log(user.value, avatarFile.value);
+    if (user.value.length < 4) {
+        userError.textContent = 'Invalid entry';
+        userError.style.color = 'red';
+        user.style.borderColor = 'red';
+        user.focus();
+    }
+
     event.preventDefault();
+    console.log(user.value, avatarFile.value);
 });

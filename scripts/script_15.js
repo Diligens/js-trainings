db5fdbf4fca993118ab6ba19529126288525ac0c
Diligens <artemrandom@yandex.ru>
2019-05-08 12:39:32 +0300

    Accessing Form Fields

--- scripts/script_15.js
+++ scripts/script_15.js
@@ -5,5 +5,9 @@ let form = document.getElementById('user-form');
 
 form.addEventListener('submit', event => {
     // prevent the browser from submitting the form
+    let user = form.elements['user'];
+    let avatarFile = form.elements['avatar-file'];
+
+    console.log(user.value, avatarFile.value);
     event.preventDefault();
 });
